c6ce35a65a6a4bd8dc389dc6a6e47111fa2e5060
Tristan Pacba <tpacba@live.com>
2020-09-29 07:07:43 -0700

    removed oauth-server folder and gitignore (for now)

---
+++ client/node_modules/@popperjs/core/dist/esm/dom-utils/contains.js
@@ -0,0 +1,24 @@
+export default function contains(parent, child) {
+  var rootNode = child.getRootNode && child.getRootNode(); // $FlowFixMe: Node is not aware of host
+
+  var isShadow = Boolean(rootNode && rootNode.host); // First, attempt with faster native method
+
+  if (parent.contains(child)) {
+    return true;
+  } // then fallback to custom implementation with Shadow DOM support
+  else if (isShadow) {
+      var next = child;
+
+      do {
+        if (next && parent.isSameNode(next)) {
+          return true;
+        } // $FlowFixMe: need a better way to handle this...
+
+
+        next = next.parentNode || next.host;
+      } while (next);
+    } // Give up, the result is false
+
+
+  return false;
+}
