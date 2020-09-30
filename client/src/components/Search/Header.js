48c01a5d43e7bec3430fa6fe563f8c2ec0fd24ce
Tristan Pacba <tpacba@live.com>
2020-09-30 11:44:36 -0700

    Revert "Will 9.29"

--- client/src/components/Search/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <h1 className="main-heading">Spotify Music Search</h1>;
-};
-
-export default Header;
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,4 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-module.exports = router; 
+module.exports = router;
