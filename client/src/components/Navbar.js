a54cc243e5c1673db93afbbb8578e57a2f6cdbf0
Richard Antolin <richardantolin@gmail.com>
2020-09-30 20:38:10 -0700

    added icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,10 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Landanimsmall from '../components/Landanimsmall';
+import HouseIcon from '@material-ui/icons/House';
+import SearchIcon from '@material-ui/icons/Search';
+import Mood from '@material-ui/icons/SentimentSatisfiedAlt';
+import Account from '@material-ui/icons/AccountBox';
 
 const useStyles = makeStyles({
     root: {
@@ -17,18 +21,30 @@ export default function () {
     return (
         <div>
         <Paper className={classes.root}>
-        <Landanimsmall></Landanimsmall>
+        <center><Landanimsmall/></center>
+    
             <Tabs
                 aligncontent="center"
                 indicatorColor="primary"
                 textColor="primary"
                 centered
             >
-                <Tab label="Home" href={"/"} />
-                <Tab label="Search" href={"/search"} />
-                <Tab label="Search Mood" href={"/search/mood"} />
-                <Tab label="Pet Music" href={"/petmusic"} />
-                <Tab label="Account" href={"/account"} />
+                <a href={"/"}><HouseIcon 
+                    style={{ fontSize: 25, marginRight:'40px', color:'grey'}} 
+                    label="Home" 
+                    /></a>
+                <a href={"/search"}><SearchIcon 
+                    style={{ fontSize: 25, marginRight:'40px', color:'grey'}} 
+                    label="Search" 
+                    /></a>
+                <a href={"/search/mood"}><Mood 
+                    style={{ fontSize: 25, marginRight:'40px', color:'grey'}} 
+                    label="Search Mood" 
+                    /></a>
+                <a href={"/account"}><Account
+                    style={{ fontSize: 25, marginRight:'40px', color:'grey'}} 
+                    label="Account" 
+                    /></a>    
             </Tabs>
         </Paper>
         </div>
