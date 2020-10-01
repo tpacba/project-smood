ae52d6a7df0edb10b9ad4c8f2931a12b9ce2af68
William Flores <williamsffc@gmail.com>
2020-10-01 14:53:20 -0700

    search artist function

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -12,6 +12,7 @@ class Search extends React.Component {
     search: "",
     results: [],
     error: "",
+    type: "track",
   };
 
   componentDidMount() {
@@ -22,40 +23,49 @@ class Search extends React.Component {
     const token = sessionStorage.getItem("token");
     if (token) {
       spotifyApi.setAccessToken(token);
-      this.setState({ loggedIn: true })
+      this.setState({ loggedIn: true });
     }
-  }
+  };
 
   handleInputChange = (event) => {
     this.setState({ search: event.target.value });
   };
 
+  handleTypeInput = (event) => {
+    this.setState({ type: event.target.value });
+   
+    console.log("Hello")
+  };
+
   handleFormSubmit = (event) => {
     event.preventDefault();
-    spotifyApi.search(this.state.search, ["track", "artist"])
+    console.log(this.state.type)
+    spotifyApi
+      .search(this.state.search, [this.state.type])
       .then((res) => {
-        console.log(res)
-        this.setState({ results: res.artists.items });
+        console.log(res);
+        this.setState({ results: res[this.state.type + "s"].items });
       })
-      .catch(err => this.setState({ error: err.message }));
+      .catch((err) => this.setState({ error: err.message }));
   };
 
   render() {
-    console.log(this.state)
+  
     return (
       <div>
-        {!this.state.loggedIn &&
-          <Login></Login>
-        }
-        {this.state.loggedIn &&
+        {!this.state.loggedIn && <Login></Login>}
+        {this.state.loggedIn && (
           <div>
             <SearchForm
               handleFormSubmit={this.handleFormSubmit}
               handleInputChange={this.handleInputChange}
+              handleTypeInput={this.handleTypeInput}
             />
-            <SearchResults results={this.state.results} />
+            {this.state.results ? (
+              <SearchResults type={this.state.type} results={this.state.results} />
+            ) : (<div></div>)}
           </div>
-        }
+        )}
       </div>
     );
   }
