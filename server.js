75e258d81c3585982ff2c5bd4befc15b83149518
Tristan Pacba <tpacba@live.com>
2020-10-05 09:52:30 -0700

    changes to deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(express.static("public"));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build'));
 });
 
