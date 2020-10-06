11141c9dd682ba5b7c8439b7a79a48108d7a5e7c
Tristan Pacba <tpacba@live.com>
2020-10-06 12:53:35 -0700

    changed back from hash

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-router.use("/#/api", apiRoutes);
+router.use("/api", apiRoutes);
 
 router.use((req, res) =>
     res.sendFile(path.join(__dirname, "../client/build/index.html"))
